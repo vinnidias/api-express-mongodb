ee36d8675973600b48449dc6e4e6bf11a4328ee3
vinnidias <vinicius.3991@gmail.com>
2022-07-21 18:23:04 -0300

    express app initial config

--- index.ts
+++ index.ts
@@ -1,5 +1,49 @@
 //config inicial 
 
+require('dotenv/config');
 import express from "express";
+import mongoose from "mongoose";
+
+const customerRoutes = require("./routes/customerRoutes")
+
 
 const app = express();
+
+const dbKey = process.env.DB_KEY
+const dbUser = process.env.DB_USER
+
+
+//Lendo JSON / middlewares
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.use(express.json());
+
+app.use("/customer", customerRoutes)
+
+
+// rota inicial/enpoint
+
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" })
+
+})
+
+//mogo db conection 
+
+//
+
+//eviar a porta
+mongoose.connect(`mongodb+srv://${dbUser}:${dbKey}@cluster0.nbr0s.mongodb.net/?retryWrites=true&w=majority`)
+  .then(() => {
+    app.listen(3000);
+    console.log("mongodb is connected")
+  })
+  .catch((err) => console.log("mongodb connect fail:", err))
+
