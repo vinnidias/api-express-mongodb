4bfe6a421cb98d74741efe2656e421f1acc47be5
vinnidias <vinicius.3991@gmail.com>
2022-07-21 19:14:19 -0300

    RESTFULL routes created

--- routes/customerRoutes.ts
+++ routes/customerRoutes.ts
@@ -2,25 +2,94 @@ const router = require("express").Router();
 import { Request, Response } from "express";
 import { Customer } from "../models/Customer";
 
-router.post("/", async (req: Request, res: Response)=> {
-  const {name, email, cellphone, message} = req.body
+router.post("/", async (req: Request, res: Response) => {
+  const { name, email, cellphone, message } = req.body
 
-  if(!name || !email || !cellphone) {
-    res.status(422).json({message: "some required data missed!"})
+  if (!name || !email || !cellphone) {
+    res.status(422).json({ message: "some required data missed!" })
+    return
   }
 
   const customer = {
-    name, 
-    email, 
-    cellphone, 
+    name,
+    email,
+    cellphone,
     message
   }
 
   try {
     await Customer.create(customer)
-    res.status(201).json({message: "Customer create sucess!"})
+    res.status(201).json({ message: "Customer create sucess!" })
   } catch (error) {
-    res.status(500).json({message: "Server problems! Try later"})
+    res.status(500).json({ message: "Server problems! Try later" })
+    console.log("create fail: ", error)
+  }
+})
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const allCustomers = await Customer.find();
+    res.status(200).json(allCustomers)
+  } catch (error) {
+    res.status(500).json({ message: "Server problems! Try later" })
+    console.log("create fail: ", error)
+  }
+})
+
+
+router.get("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id
+
+  try {
+    const customer = await Customer.findOne({ _id: id });
+    if (!customer) {
+      res.status(422).json({ message: "Invalid ID, customer not found!" });
+      return
+    }
+    res.status(200).json(customer)
+  } catch (error) {
+    res.status(500).json({ message: "Server problems! Try later" })
+    console.log("create fail: ", error)
+  }
+})
+
+
+router.patch("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id
+
+  const { name, email, cellphone, message } = req.body
+  const customer = { name, email, cellphone, message }
+
+  try {
+    const updateCustomer = await Customer.updateOne({ _id: id }, customer)
+
+    if (updateCustomer.matchedCount === 0) {
+      res.status(422).json({ message: "Customer not found!" });
+      return
+    }
+
+    res.status(200).json(customer)
+  } catch (error) {
+    res.status(500).json({ message: "Server problems! Try later" })
+    console.log("create fail: ", error)
+  }
+
+})
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id
+  const customer = await Customer.findOne({ _id: id });
+  if (!customer) {
+    res.status(422).json({ message: "Customer not found!" });
+    return
+  }
+
+  try {
+    await Customer.deleteOne({ _id: id });
+
+    res.status(200).json({ message: "Customer successfully removed" })
+  } catch (error) {
+    res.status(500).json({ message: "Server problems! Try later" })
     console.log("create fail: ", error)
   }
 })
