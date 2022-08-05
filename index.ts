f7aa1c31588aa8e8e6ecd225d288b67967655703
vinnidias <vinicius.3991@gmail.com>
2022-08-04 23:39:59 -0300

    adding cors config

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@
 require('dotenv/config');
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors"
 
 const customerRoutes = require("./routes/customerRoutes");
 const registerUserRouter = require("./routes/registerUserRoute");
@@ -24,13 +25,25 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET, PUT, POST,DELETE');
+    
+    next();
+});
+
+app.use(cors());
+
+
 app.use(express.json());
 
 app.use("/customer", customerRoutes);
 
 app.use("/auth/register", registerUserRouter);
 
-app.use("/auth/login", authLoginRoute)
+app.use("/auth/login" , authLoginRoute)
 
 
 // rota inicial/enpoint
